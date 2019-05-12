1a24d0b56ef19ccafca021234caddf0f897faaf8
chenghui.zhang <zhangchenghui.dev@gmail.com>
2019-05-13 03:16:39 +0800

    no message

--- src/main/java/com/objcoding/transaction/TransactionApplication.java
+++ src/main/java/com/objcoding/transaction/TransactionApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -20,43 +21,68 @@ public class TransactionApplication implements CommandLineRunner {
     @Autowired
     private ForupdateMapper forupdateMapper;
 
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+
     CountDownLatch countDownLatch = new CountDownLatch(1);
     ReentrantLock reentrantLock = new ReentrantLock();
 
+
+//    // for update加事务，并且不提交事务
 //    @Override
 //    public void run(String... args) throws Exception {
 //
 //        reentrantLock.lock();
 //
 //        new Thread(() -> {
-//            forupdate();
+//            transactionTemplate.execute(transactionStatus -> {
+//                this.forupdateMapper.findByName("testforupdate");
+//                System.out.println("==========for update==========");
+//                countDownLatch.countDown();
+//                reentrantLock.lock(); // 阻塞不让提交事务
+//                return null;
+//            });
 //        }).start();
 //
 //        countDownLatch.await();
-//        this.forupdateMapper.updateByState("CA");
+//        System.out.println("==========for update has countdown==========");
+//        this.forupdateMapper.updateByName("testforupdate");
 //        System.out.println("==========update success==========");
 //
 //        reentrantLock.unlock();
 //    }
 
+
+    // for update不加事务
     @Override
     public void run(String... args) throws Exception {
 
-        AtomicInteger atomicInteger = new AtomicInteger();
-
-        for (int i = 0; i < 10; i++) {
+        new Thread(() -> {
             this.forupdateMapper.findByName("testforupdate");
-            System.out.println("========ok:" + atomicInteger.getAndIncrement());
-        }
+            System.out.println("==========for update==========");
+            countDownLatch.countDown();
+        }).start();
+
+        countDownLatch.await();
+        System.out.println("==========for update has countdown==========");
+        this.forupdateMapper.updateByName("testforupdate");
+        System.out.println("==========update success==========");
 
     }
 
-    @Transactional
-    public void forupdate() {
-        this.forupdateMapper.findByName("testforupdate");
-        System.out.println("==========for update==========");
-        countDownLatch.countDown();
-        reentrantLock.lock();
 
-    }
+    // 并发执行for udpate
+//    @Override
+//    public void run(String... args) throws Exception {
+//
+//        AtomicInteger atomicInteger = new AtomicInteger();
+//
+//        for (int i = 0; i < 100; i++) {
+//            this.forupdateMapper.findByName("testforupdate");
+//            System.out.println("========ok:" + atomicInteger.getAndIncrement());
+//        }
+
+//    }
+
+
 }
